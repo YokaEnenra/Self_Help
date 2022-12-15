7f933458c651f7647b6f9dffb8fa5e04eb639312
YokaEnenra <lartaman1337@gmail.com>
2022-12-15 19:53:25 +0200

    Fixed typos Updated admin.py tests.py and views.py in accordance to python rules

--- src/urls.py
+++ src/urls.py
@@ -38,8 +38,8 @@ schema_view = get_schema_view(
 urlpatterns = [
 
 ] + i18n_patterns(
-# swagger
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),name='schema-json'),
+    # swagger
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # admin controls
