cbae2973b1803271da047edb053948a7d79e00c7
YokaEnenra <lartaman1337@gmail.com>
2022-12-24 00:16:35 +0200

    Removed unused import

--- Self_Help/models.py
+++ Self_Help/models.py
@@ -3,7 +3,7 @@ from random import choice
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
